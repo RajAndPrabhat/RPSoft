2604adf2dc3ff9b486836ce505a8849e5b794e4b
Prabhat Kiran <prabhat@binmile.com>
2025-01-07 14:45:05 +0530

    Add Aboutus and contact us page code

--- core/views.py
+++ core/views.py
@@ -3,8 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Welcome to RPSoft Solutions</h1>")
+    file_path="core/pages/home.html"
+    context={
+        'title':'Home'
+    }
+    return render(request, file_path, context)
 
 
 def about(request):
-    return HttpResponse("<h1>Welcome to RPSoft Solutions About Page</h1>")
+    file_path="core/pages/about.html"
+    context={
+        'title':'About Us'
+    }
+    return render(request, file_path, context)
+
+def contact(request):
+    file_path="core/pages/contact.html"
+    context={
+        'title':'Contact Us'
+    }
+    return render(request, file_path, context)
